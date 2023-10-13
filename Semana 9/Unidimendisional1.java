fb4ff03ecbccd00e4af898306cfa19eb16c00d54
nazah29 <franconazariogomez@gmail.com>
2023-10-13 11:42:08 -0500

    cambios

---
+++ Semana 9/Unidimendisional1.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Unidimendisional1 {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int opcion, auxiliar;
+        int longitud;
+        int[] arreglo;
+        System.out.println("Ingrese la longitud de los datos que desea ingresar");
+        longitud = lector.nextInt();
+        arreglo = new int[longitud];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese el numero para la posicion " + i);
+            arreglo[i] = lector.nextInt();
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Poscicion " + i + ":" + arreglo[i]);
+        }
+        System.out.println("""
+                Elija la opcion de su orden
+                [1] Ascendente
+                [2] Descendente
+                """);
+        opcion = lector.nextInt();
+        switch (opcion) {
+            case 1:
+                Arrays.sort(arreglo);
+                for (int contador : arreglo) {
+
+                }
+                System.out.println(Arrays.toString(arreglo));
+
+                break;
+            case 2:
+                for (int i = 0; i < arreglo.length - 1; i++) {
+                    for (int j = i + 1; j < arreglo.length; j++) {
+                        if (arreglo[i] < arreglo[j]) {
+                            auxiliar = arreglo[i];
+                            arreglo[i] = arreglo[j];
+                            arreglo[j] = auxiliar;
+
+                        }
+
+                    }
+
+                }
+                System.out.println(Arrays.toString(arreglo));
+                break;
+
+        }
+
+    }
+
+}
---
+++ Semana 9/Unidimensional2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Unidimensional2 {
+    public static void main(String[] args) {
+        // Resolver un problema que solicite al usuario N datos que serán
+        // almacenados en arreglos paralelos y muestre un reporte ASCII
+        // con dichos datos. Incluir algún cálculo con contadores y
+        // acumuladores.
+        Scanner lector = new Scanner(System.in);
+        int longitud;
+        double precios;
+        String[] arregloprecio, arreglo;
+        System.out.println("Ingresa la cantidada de componentes que tengas en tu pc");
+        longitud = lector.nextInt();
+        lector.nextLine();
+        arreglo = new String[longitud];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese el componente numero " + i);
+            arreglo[i] = lector.nextLine();
+        }
+        arregloprecio = new String[longitud];
+        System.out.println("Ahora ingresa los precios");
+        for (int j = 0; j < arreglo.length; j++) {
+            System.out.println("Ingresa el precio en la poscicion numero" + j);
+            arregloprecio[j] = lector.nextLine();
+        }
+
+       
+        System.out.println("Recibo");
+        System.out.println("**************************");
+         for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Poscicion " + i + ":" + arreglo[i]);
+        }
+        System.out.println("***************************************");
+        for (int j = 0; j < arreglo.length; j++) {
+            System.out.print("Poscicion " + j + ":" + arregloprecio[j]);
+
+        }
+
+    }
+}
